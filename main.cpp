f929e99ef5fd02ac53b8a3ef49023f9eb65dc6bf
UBC Student <chancelp@students.cs.ubc.ca>
2024-12-29 15:17:35 -0330

    i give up

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "httplib.h"
 #include <iostream>
+#include <stdlib.h>
 
 httplib::Server svr;
 
@@ -9,37 +10,29 @@ int main() {
 		std::string domain = req.matches[1];
 		std::string file = req.matches[2];
 		std::cout << domain << std::endl << file << std::endl;
-		try {
-			httplib::Client cli(domain);
-			cli.set_follow_location(true);
-			volatile std::string out = "";
-			volatile bool downloading = true;
-			volatile bool hold = false;
-			res.set_chunked_content_provider(get->headers->find("Content-Type"),
-				[&](size_t offset, httplib::DataSink &sink) {
-					bool temp_downloading = true;
-					std::string write = "";
-					while (temp_downloading) {
-						hold = true;
-						write = out;
-						out = "";
-						hold = false;
-						if (write.size() != 0)
-							sink.write(write, write.size());
-					}
-      				sink.done();
+		httplib::Client cli_head(domain);
+		cli_head.set_follow_location(true);
+		auto head = cli_head.Head(file);
+		res.set_content_provider(atoll(head->headers.find("Content-Length")->first.c_str()),
+			head->headers.find("Content-Type")->first,
+			[&, domain, file](size_t offset, size_t length, httplib::DataSink &sink) {
+				httplib::DataSink* toWrite = &sink;
+				httplib::Client cli(domain);
+				cli.set_follow_location(true);
+				std::cout << "got sink" << std::endl;
+				volatile size_t i = 0;
+				auto get = cli.Get(file,
+					[&](const char *data, size_t data_length) {
+						std::cout << "writing" << std::endl;
+						toWrite->write(data, data_length);
+						i += data_length;
+						return true;
+					});
+				std::cout << "sent request" << std::endl;
+				while (i < length) usleep(1000);
 				return true;
-			}
-						
-			auto get = cli.Get(file	[&](const char *data, size_t data_length) {
-				while (hold); // Stall
-				out.append(data, data_length);
-				return true;
-			}));
-`			downloading = false;
-		} catch (...) {
-			return res.status = httplib::StatusCode::NotAcceptable_406;
-		}
+			});
+		return res.status = httplib::StatusCode::OK_200;
 	});
 	svr.listen("0.0.0.0", 8080);
 	return 0;
